4f19cef1d30eb5c332cf0a2a5da90b0cf0d457f3
KevinVSantos01 <kevinvsantos01@gmail.com>
2025-10-09 03:32:09 -0300

    updateApi

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/KevinVSantos01/projeto-final-JS/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
